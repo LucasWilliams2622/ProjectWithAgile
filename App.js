c89b0805256f650c5dc6b65db9256d1344bca7bd
LocDayNha <vohoangloc200@gmail.com>
2023-05-25 12:59:13 +0700

    fix initialRouteName="Login"

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ import PieChartScreen from './screens/TestTab/PieChartScreen'
 const Stack = createNativeStackNavigator();
 const StackBegin = () => {
   return (
-    <Stack.Navigator initialRouteName="ChangePassword" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
 
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Register" component={Register} />
