5b838e54c316ac3ae3cc534eaaac00b305e0086a
Lucas_Williams <101234675+LucasWilliams2622@users.noreply.github.com>
2023-06-01 18:17:05 +0700

    fix UI chart

--- App.js
+++ App.js
@@ -35,7 +35,7 @@ import messaging from '@react-native-firebase/messaging';
 const Stack = createNativeStackNavigator();
 const StackBegin = () => {
   return (
-    <Stack.Navigator initialRouteName="Register" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Welcome" component={Welcome} />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Register" component={Register} />
@@ -44,7 +44,6 @@ const StackBegin = () => {
       <Stack.Screen name="SignCode" component={SignCode} />
 
 
-      
     </Stack.Navigator>
   )
 }
@@ -69,12 +68,12 @@ const App = () => {
   }, []);
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="StackBegin" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Chart" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="StackBegin" component={StackBegin} />
         <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen name="BottomTabs" component={BottomTabs} />
         <Stack.Screen name="AddNew" component={AddNew} />
-        <Stack.Screen name="Setting" component={Setting} />
+        <Stack.Screen name="Chart" component={Chart} />
         <Stack.Screen name="TestPicker" component={TestPicker} />
        </Stack.Navigator>
      </NavigationContainer>
--- screens/MainTabs/Chart.js
+++ screens/MainTabs/Chart.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,Dimensions } from 'react-native'
+import { StyleSheet, Text, View, Dimensions, SafeAreaView } from 'react-native'
 import React from 'react'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import ItemYear from '../../component/ItemYear';
@@ -11,38 +11,60 @@ const windowWidth = Dimensions.get('window').width;
 
 const Chart = () => {
   return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.boxTitle}>
+        <Text style={styles.title}>Phân tích chi tiêu</Text>
+      </View>
 
-    <Tab.Navigator 
-    tabBarShowLabel
-            // tabBarPressColor='white'
-            tabBarActiveTintColor='red'
-            tabBarShowIcon={true}
-            keyboardDismissMode='interactive'
-            backBehavior='initialRoute'
-            screenOptions={{
-                tabBarLabelStyle: { fontSize: 14 },
-                tabBarItemStyle: { width: windowWidth / 2 },
-               tabBarStyle: { backgroundColor: COLOR.primary },
-                swipeEnabled:true,
-            
-            }}
-           
-            tabBarOptions={{
-                activeTintColor: 'white',
-                inactiveTintColor: 'white',
-                indicatorStyle: {
-                    backgroundColor: 'white',
-                },
-              }}
-            initialRouteName="ItemMonth"
-    >
-      <Tab.Screen name="Theo Tháng" component={ItemMonth} />
-      <Tab.Screen name="Theo Năm" component={ItemYear} />
-    </Tab.Navigator>
+      <Tab.Navigator
+        tabBarShowLabel
+        // tabBarPressColor='white'
+
+        tabBarActiveTintColor='red'
+        tabBarShowIcon={true}
+        keyboardDismissMode='interactive'
+        backBehavior='initialRoute'
+        screenOptions={{
+          tabBarItemStyle: { width: windowWidth / 2 },
+          tabBarStyle: { backgroundColor: COLOR.primary },
+          swipeEnabled: true,
+          tabBarLabelStyle: { fontSize: 16, fontWeight: '400' },
+          tabBarActiveTintColor: 'white',
+          tabBarInactiveTintColor: 'white',
+          tabBarIndicatorStyle: {
+            backgroundColor: 'white',
+          },
+        }}
+        initialRouteName="ItemMonth"
+      >
+        <Tab.Screen name="Theo Tháng" component={ItemMonth} />
+        <Tab.Screen name="Theo Năm" component={ItemYear} />
+      </Tab.Navigator>
+    </SafeAreaView>
 
   )
 }
 
 export default Chart
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  boxTitle: {
+    height: '7%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: COLOR.primary,
+
+  },
+  title: {
+    marginLeft: 15,
+    color: COLOR.white,
+    fontWeight: '500',
+    fontSize: 20,
+    fontStyle: 'normal'
+
+  }
+})
